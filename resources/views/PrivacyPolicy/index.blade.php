7702b0fd56bfbb3acfe061c5613ff983e2a1217e
marcosilvadf <marcosilva18contato@gmail.com>
2024-09-14 20:27:13 -0300

    first-commit

---
+++ resources/views/PrivacyPolicy/index.blade.php
@@ -0,0 +1,33 @@
+@extends('Default.layout')
+@slot('title')
+    Início
+@endslot
+@section('title', 'Início')
+
+@section('main')
+    <h1>Política de Privacidade</h1>
+    <hr>
+
+    <h2>Esta Política de Privacidade descreve como o aplicativo ("ButtonBox By Maiorzin") coleta, usa e protege suas informações. Ao utilizar o Aplicativo, você concorda com as práticas descritas nesta política.</h2>
+
+    <h3>1. Coleta de Informações</h3>
+    <p>O Aplicativo não coleta, armazena ou compartilha dados pessoais identificáveis de seus usuários. A única informação que o Aplicativo pode acessar é o IP local da máquina à qual o celular está se conectando para funcionar como um botão de controle. Este dado é utilizado exclusivamente para fins de conexão e não é armazenado ou compartilhado com terceiros.</p>    
+
+    <h3>2. Uso de Informações</h3>
+    <p>O IP local da máquina é utilizado apenas para estabelecer a comunicação entre o dispositivo móvel e o computador, permitindo que o Aplicativo funcione conforme o esperado. Nenhum dado pessoal é coletado ou processado pelo Aplicativo.</p>
+
+    <h3>3. Anúncios</h3>
+    <p>O Aplicativo exibe anúncios através do Google AdMob. O Google AdMob pode coletar dados para fornecer anúncios personalizados ou baseados em interesses. Recomendamos que você revise a Política de Privacidade do Google AdMob para entender melhor como eles utilizam suas informações.</p>
+
+    <h3>4. Compartilhamento de Informações</h3>
+    <p>O Aplicativo não compartilha nenhum dado com terceiros. O IP local é utilizado apenas localmente e não é transmitido para servidores externos.</p>
+
+    <h3>5. Segurança</h3>
+    <p>Tomamos medidas para garantir que quaisquer informações que o Aplicativo utilize, como o IP local, sejam tratadas com segurança e não sejam acessíveis a terceiros.</p>
+
+    <h3>6. Alterações a Esta Política de Privacidade</h3>
+    <p>Esta política pode ser atualizada periodicamente. Recomendamos que os usuários revisem esta página regularmente para se manterem informados sobre quaisquer alterações. O uso contínuo do Aplicativo após as alterações será considerado como aceitação das mesmas.</p>
+
+    <h3>7. Contato</h3>
+    <p>Se você tiver quaisquer perguntas ou preocupações sobre esta Política de Privacidade, entre em contato conosco através do e-mail <a href="mailto:marcosilva18contato@gmail.com">marcosilva18contato@gmail.com</a>.</p>
+@endsection
