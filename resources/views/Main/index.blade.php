85f7c9e4f68d0d6bb12367202fca8d06ef149be4
marcosilvadf <marcosilva18contato@gmail.com>
2025-04-27 11:26:59 -0300

    feat: new version v5

--- resources/views/Main/index.blade.php
+++ resources/views/Main/index.blade.php
@@ -82,6 +82,13 @@
 
         <p style="font-weight: bolder">Clique <a href="https://play.google.com/apps/testing/br.com.buttonbox" target="_blank">aqui para entrar no teste e fazer o Download do app.</a></p>
 
+        <hr>
+
+        <h2>Programas e arquivos necessários para o PC</h2>
+
+        <p style="font-weight: bolder"><a href="{{asset('programsv5/buttonboxbymaiorzin.zip')}}">Clique aqui para baixar o Button Box.</a></p>
+        <hr>
+
         <div style="position: relative; width: 100%; padding-top: 56.25%;">
             <iframe 
                 src="https://www.youtube.com/embed/vOdB5KYLeGs?si=zk-JZ-f1QsoHHsW9" 
@@ -108,152 +115,6 @@
         <p>Depois quando a página atualizar e ficar igual a imagem abaixo, clique no link azul, o mesmo que está circulado na imagem abaixo e pronto, já poderá baixar direto da Play Store.</p>
         <hr>
         <img class="img_clicked" src="{{asset('images/setup_become_tester/app_abrir_pagina_ps.jpg')}}" alt="">
-
-        <hr>
-
-        <h2>Programas e arquivos necessários para o PC</h2>
-
-        <p style="font-weight: bolder"><a href="{{asset('programsv4/vJoySetup.exe')}}">Clique aqui para baixar o VJoy</a></p>
-        <p style="font-weight: bolder"><a href="{{asset('programsv4/SimHubSetup_9.4.4.exe')}}">Clique aqui para baixar o SimHub</a></p>
-        <p style="font-weight: bolder"><a href="{{asset('programsv4/test.simhubdash')}}">Clique aqui para baixar o Dashboard para o SimHub</a></p>
-        <p style="font-weight: bolder"><a href="{{asset('programsv4/ets2controllerv4.zip')}}">Clique aqui para baixar o ETS2Controller</a></p>
-        <hr>
-        <h4>Para instalar o vJoy basta clicar no <span style="font-weight: bolder">vJoySetup.exe</span>, na tela de setup clique em <span style="font-weight: bolder">next</span>:</h4>
-        <hr>
-
-        <img class="img_clicked" src="{{asset('images/setup_vjoy/setup_vjoy.JPG')}}" alt="">
-
-        <hr>
-        <p>Depois clique em <span style="font-weight: bolder">next</span>:</p>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_vjoy/setup_vjoy2.JPG')}}" alt="">
-
-        <hr>
-        <p>Depois clique em <span style="font-weight: bolder">next</span>:</p>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_vjoy/setup_vjoy3.JPG')}}" alt="">
-
-        <hr>
-        <p>Depois clique em <span style="font-weight: bolder">OK</span>:</p>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_vjoy/setup_vjoy4.JPG')}}" alt="">
-
-        <hr>
-        <h5>Configuração do vJoy, logo após, abra o programa <span style="font-weight: bolder">Configure vJoy</span>, deixe as opções iguais a imagem abaixo e clique em <span style="font-weight: bolder">apply</span>:</h5>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_vjoy/setup_vjoy_configuracao_padrao.JPG')}}" alt="">
-
-        <hr>
-        <h4>Para instalar o SimHub basta clicar no <span style="font-weight: bolder">SimHubSetup_9.4.4.exe</span>, na tela de setup clique em <span style="font-weight: bolder">next</span>:</h4>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_simhub/setup1.JPG')}}" alt="">
-
-        <hr>
-        <p>Depois clique em <span style="font-weight: bolder">next</span>:</p>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_simhub/setup2.JPG')}}" alt="">
-
-        <hr>
-        <p>Depois clique em <span style="font-weight: bolder">next</span>:</p>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_simhub/setup3.JPG')}}" alt="">
-
-        <hr>
-        <p>Depois clique em <span style="font-weight: bolder">install</span>, essa etapa no meu computador demorou mais de 5 minutos para instalar:</p>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_simhub/setup4.JPG')}}" alt="">
-
-        <hr>
-        <p>Depois clique em <span style="font-weight: bolder">Finish</span>:</p>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_simhub/setup5_launch.JPG')}}" alt="">
-
-        <hr>
-        <h5>Configuração do SimHub, quando o programa abrir, deixe as configurações como está na imagem abaixo, no meu caso já estava assim e clique em <span style="font-weight: bolder">OK</span>:</h5>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_simhub/setup6.JPG')}}" alt="">
-
-        <hr>
-        <p>Essa tela é pra seleção de jogos, ele vem com todos marcados, se você preferir pode desabilitar o que quiser, menos o ETS2, depois clique em <span style="font-weight: bolder">OK</span>:</p>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_simhub/setup7.JPG')}}" alt="">
-
-        <hr>
-        <p>Procure o ETS2 e <span style="font-weight: bolder">clique nele duas vezes</span>:</p>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_simhub/setup8.JPG')}}" alt="">
-
-        <hr>
-        <p>Depois, clique em <span style="font-weight: bolder">Dash Studio</span> no menu e em seguida clique em <span style="font-weight: bolder">Import dashboard:</span></p>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_simhub/setup9.JPG')}}" alt="">
-
-        <hr>
-        <p>Irá abrir um explorer, no explorer que abrir, vá até o arquivo que você baixou chamado <span style="font-weight: bolder">test.simhubdash</span>, clique e selecione o arquivo, e por fim, clique em Import into SimHub library:</p>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_simhub/setup10.JPG')}}" alt="">
-
-        <hr>
-        <p>Depois clique <em></em><span style="font-weight: bolder">OK</span>:</p>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_simhub/setup11.JPG')}}" alt="">
-
-        <hr>
-        <p>Após importar o arquivo corretamente, clique em <span style="font-weight: bolder">Devices</span> no menu e depois em <span style="font-weight: bolder">Add Device</span>:</p>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_simhub/setup12.JPG')}}" alt="">
-
-        <hr>
-        <p>Na barra de pesquisa digite <span style="font-weight: bolder">web</span> e selecione <span style="font-weight: bolder">Web device slot (Phone, tablet, app...)</span> e clique em <span style="font-weight: bolder">OK</span>:</p>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_simhub/setup13.JPG')}}" alt="">
-
-        <hr>
-        <p>Em seguida, clique na lista de <span style="font-weight: bolder">Main dashboard</span> e selecione o <span style="font-weight: bolder">test</span>:</p>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_simhub/setup14.JPG')}}" alt="">
-
-        <hr>
-        <p>Tudo pronto, lembre-se de quando for jogar deixe o SimHub aberto!</p>
-        <hr>
-
-        <hr>
-        <h4>Para instalar o SimHub basta clicar no <span style="font-weight: bolder">SimHubSetup_9.4.4.exe</span>, na tela de setup clique em <span style="font-weight: bolder">next</span>:</h4>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_simhub/setup1.JPG')}}" alt="">
-
-        <hr>
-        <h4>Descompacte o arquivo <span style="font-weight: bolder">ets2controllerv4.zip</span>, recomendo que você crie um atalho na Área de trabalho.</h4>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_ets2controller/config_ets2.JPG')}}" alt="">
-
-        <hr>
-        <h4>Toda vez que for jogar, abra o executável <span style="font-weight: bolder">ets2controller.exe</span>:</h4>
-        <hr>
-        <img id="img" class="img_clicked" src="{{asset('images/setup_ets2controller/config_ets2_1.JPG')}}" alt="">
-
-        <hr>
-        <p>Caso mostre o aviso da imagem abaixo, clique em <span style="font-weight: bolder">Mais informações</span>:</p>
-        <hr>
-        <img id="img" class="img_clicked" src="{{asset('images/setup_ets2controller/aviso1.JPG')}}" alt="">
-
-        <hr>
-        <p>Depois clique em <span style="font-weight: bolder">Executar mesmo assim</span>:</p>
-        <hr>
-        <img id="img" class="img_clicked" src="{{asset('images/setup_ets2controller/aviso2.JPG')}}" alt="">
-
-        <hr>
-        <p>E caso mostre essa em seguida, deixe como está na imagem abaixo e clique em <span style="font-weight: bolder">Permitir acesso</span>:</p>
-        <hr>
-        <img id="img" class="img_clicked" src="{{asset('images/setup_ets2controller/aviso3.JPG')}}" alt="">
-
-        <hr>
-        <h5 style="font-weight: bolder">Reforço que sempre use os programas e app baixados desse site que é o oficial, caso você baixe de outro lugar, pode haver códigos mal intencionados escondidos.</h5>
-
-        <hr>
-        <p>O número do <span style="font-weight: bolder">IP</span> é o que você deve colocar no app pra Android, no meu caso está aparecendo 2 porquê no meu notebook tem duas placas de rede, wi-fi e ethernet, nesse caso você deve colocar a que você está usando no momento, wireless é o wi-fi e ethernet é o cabo, lembre-se, são os números com os pontos, exemplo: <span style="font-weight: bolder">192.168.1.36</span></p>
-        <hr>
-        <img class="img_clicked" src="{{asset('images/setup_ets2controller/config_ets2_2.JPG')}}" alt="">
 
         @section('js')
         <script>
