72ae6e26659a84a75ea1f038e93b61ef713183bf
marcosilvadf <marcosilva18contato@gmail.com>
2026-04-03 10:51:56 -0300

    feat: add icons parking and hazard

---
+++ resources/views/icons/first/hazard.blade.php
@@ -0,0 +1,22 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 512 512">
+  <defs>
+    <style>
+      .cls-1 {
+        fill: none;
+        stroke: silver;
+        stroke-width: 25px;
+        fill-rule: evenodd;
+      }
+
+      .cls-2 {
+        font-size: 72px;
+        fill: silver;
+        text-anchor: middle;
+        font-family: Impact;
+      }
+    </style>
+  </defs>
+  <path id="main" class="cls-1" d="M256,68L473.375,444.5H38.625Z"/>
+  <path id="center" class="cls-1" d="M256.6,143.888L411.457,412.115H101.734Z"/>
+  <text id="alert" class="cls-2" transform="matrix(2.132, 0, 0, 2.132, 256, 360.422)"><tspan x="0">!</tspan></text>
+</svg>
