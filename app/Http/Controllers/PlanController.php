34dd1f806188eb82df651d74366d51c39e042277
marcosilvadf <marcosilva18contato@gmail.com>
2026-04-23 10:55:37 -0300

    feat: return payment and menu

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -12,9 +12,7 @@ class PlanController extends Controller
     }
 
     public function payment()
-    {
-        return abort(404);
-        
+    {   
         $preferenceId = null;
 
         $mercadoPagoService = new MercadoPagoService;
