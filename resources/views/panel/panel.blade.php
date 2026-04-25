76f4d4bca628f02990f6c9bc13ec1335613b20bd
marcosilvadf <marcosilva18contato@gmail.com>
2026-04-25 12:36:47 -0300

    feat: add fontawesome

--- resources/views/panel/panel.blade.php
+++ resources/views/panel/panel.blade.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Truck Dashboard</title>
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
 <style>
 
 {!! $panel->css !!}
@@ -92,6 +92,6 @@ function toggleFullScreen() {
 }
 
 </script> --}}
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/js/all.min.js"></script>
 </body>
 </html>
